cc4c77a8a3a8b44ba397fd1acb0d155367f189f2
Rafael Toresani <rtoresani@gmail.com>
2024-08-18 09:27:10 -0300

    initial commit

---
+++ src/main/java/com/rtoresani/UrbanSneakApplication.java
@@ -0,0 +1,13 @@
+package com.rtoresani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanSneakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanSneakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rtoresani/UrbanSneakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rtoresani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanSneakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
